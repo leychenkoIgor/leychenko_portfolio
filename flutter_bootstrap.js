6fdc20a324cbff01e0bfbf0994a9a8710e1d01a7
thorleichenko <leychenko.igor@gmail.com>
2024-06-10 10:54:00 +0300

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3378128064"
+    serviceWorkerVersion: "3278926923"
   }
 });
